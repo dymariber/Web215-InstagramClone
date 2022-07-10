37818a93d39fcf6efb38d10d01b32537fedf2fda
dymariber <101956056+dymariber@users.noreply.github.com>
2022-07-10 02:07:56 -0400

    Add files via upload

---
+++ instagram-frontend/build/precache-manifest.4a32df4604f77f0765a858828f2d1402.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b6f611a8f740205c13f68528c34f66d3",
+    "url": "/index.html"
+  },
+  {
+    "revision": "95fbacdb100abb6f4c7a",
+    "url": "/static/css/main.1da85e6d.chunk.css"
+  },
+  {
+    "revision": "ac7675d73c7498b74e84",
+    "url": "/static/js/2.0269f760.chunk.js"
+  },
+  {
+    "revision": "4d48a5c33a5ec0420c913ba93dfc17ce",
+    "url": "/static/js/2.0269f760.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "95fbacdb100abb6f4c7a",
+    "url": "/static/js/main.e46294b0.chunk.js"
+  },
+  {
+    "revision": "12ffa9c2a2e2bca05bc1",
+    "url": "/static/js/runtime-main.a41b20c0.js"
+  }
+]);
